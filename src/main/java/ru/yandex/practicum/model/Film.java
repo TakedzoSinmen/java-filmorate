22011a061d6c24980e2664080d042a8ddc0dc31a
Stepan4o <Stepan4oranec@gmail.com>
2024-01-18 18:04:42 +0200

    Добавили две аннотации/ тестовый коммит

--- src/main/java/ru/yandex/practicum/model/Film.java
+++ src/main/java/ru/yandex/practicum/model/Film.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import ru.yandex.practicum.model.validation.ValidReleaseDate;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,8 @@ import java.util.List;
 public class Film {
     private int id;
     private String name;
+    @NonNull
+    @Size(max = 200, message = "Description cannot be more than 200 characters")
     private String description;
     @ValidReleaseDate
     private LocalDate releaseDate;
