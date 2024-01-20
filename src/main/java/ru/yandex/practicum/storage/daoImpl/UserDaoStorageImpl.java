259b4c59a64756564a90516831b36d1cf083129e
Stepan4o <Stepan4oranec@gmail.com>
2024-01-20 17:35:16 +0200

    Изменили List на Set (Genre)

--- src/main/java/ru/yandex/practicum/storage/daoImpl/UserDaoStorageImpl.java
+++ src/main/java/ru/yandex/practicum/storage/daoImpl/UserDaoStorageImpl.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.storage.daoImpl;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
